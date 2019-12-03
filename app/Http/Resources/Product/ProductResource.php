34e0aae9032f82ac003bff896d63901a82584f83
Alamin Sarker <alamin.php100@gmail.com>
2019-12-03 10:01:57 +0600

    Modify product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,17 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
+            'totalPrice' => round((1-($this->discount/100)) * $this->price,2),
+            //Discount calculation
+            // (discount  = 17 / 100) = .17
+            // 1-calculate discount .17
+            // .83*price
+            'rating'    => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
